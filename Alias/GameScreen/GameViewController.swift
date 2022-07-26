c49c28ae0217db1d198bfe70084637b6ba824774
ForestLamp <y10b@ya.ru>
2022-07-26 23:57:27 +0300

    Добавил шрифты

--- Alias/GameScreen/GameViewController.swift
+++ Alias/GameScreen/GameViewController.swift
@@ -15,8 +15,20 @@ class GameViewController: UIViewController {
         let label = UILabel()
         label.text = "27"
         label.textColor = .black
-        label.font = .systemFont(ofSize: 72, weight: .bold)
-        label.backgroundColor = .gray
+        label.font = UIFont(name: "PhosphateRR Solid", size: 72)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var secondsTextLabel: UILabel = {
+        let label = UILabel()
+        label.text = "секунд seconds"
+        label.textColor = .black
+//        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.font = UIFont(name: "Phosphate_Pro_Inline", size: 24)
+
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         
@@ -33,13 +45,18 @@ class GameViewController: UIViewController {
     
     private func setupUI() {
         
-        view.addSubview(self.secondsLabel)
+        view.addSubview(secondsLabel)
+        view.addSubview(secondsTextLabel)
         
         NSLayoutConstraint.activate([
             
             secondsLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             secondsLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
-            secondsLabel.widthAnchor.constraint(equalToConstant: 100)
+            secondsLabel.widthAnchor.constraint(equalToConstant: 100),
+            
+            secondsTextLabel.topAnchor.constraint(equalTo: secondsLabel.bottomAnchor, constant: 5),
+            secondsTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondsTextLabel.widthAnchor.constraint(equalToConstant: 1000)
         ])
     }
 
