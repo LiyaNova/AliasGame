d1d443a0a25402c0b94e60e0b54d47cc40bb43d7
Yuliya Filimonova <yuliya_nova@icloud.com>
2022-07-28 16:50:56 +0300

    Added level choice

---
+++ Alias/DifficultyPage/DifficultyPage.swift
@@ -0,0 +1,15 @@
+//
+//  DifficultyPage.swift
+//  Alias
+//
+//  Created by Юлия Филимонова on 28.07.2022.
+//
+
+import Foundation
+
+struct DifficultyPage {
+    let image: String
+    let level: String
+    let color: String
+    let description: String
+}
