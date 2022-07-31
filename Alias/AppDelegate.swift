48f05570880b710158ed1596f12541444896d9aa
ForestLamp <y10b@ya.ru>
2022-07-31 11:15:06 +0300

    AppDelegate

--- Alias/AppDelegate.swift
+++ Alias/AppDelegate.swift
@@ -11,9 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .white
 
-        window?.rootViewController = ScoreViewController()
-        
-        //ppNavigationController(rootViewController: TeamsMenuViewController(minNumberOfTeams: 2, maxNumberOfTeams: 10))
+        window?.rootViewController = AppNavigationController(rootViewController: TeamsMenuViewController(minNumberOfTeams: 2, maxNumberOfTeams: 10))
         
         window?.makeKeyAndVisible()
         
